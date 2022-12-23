2cd648b1213b2e06465a086244dcec64bd8d4c28
shubham-am-i <patilshubham2k21@gmail.com>
2022-12-23 07:52:57 +0530

    initial project setup

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import Table from '../components/Table'
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>Analytics</h1>
+      <Table />
+    </div>
+  )
+}
+export default HomeScreen
